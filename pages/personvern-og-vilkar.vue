13736c969eb88a90b35b577aa3b07060ba512420
asharghi <a.sharghi.mail@gmail.com>
2021-06-11 02:07:37 +0200

    nolayout fix

---
+++ pages/personvern-og-vilkar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <page-header />
+    <main class="page-content">
+      <div class="article">
+        <div class="wrapper">
+          <div class="element text-content">
+            <h1 class="heading-1 u-center">
+              Personvern og vilkår
+            </h1>
+            <!-- article -->
+            <article
+              id="post-41"
+              class="post-41 page type-page status-publish hentry"
+            >
+              <p />
+
+              <p>
+                <a href="https://personvern.okam.no/?nolayout">Personvern</a>
+              </p>
+
+              <p><a href="https://vilkar.okam.no/?nolayout">Vilkår</a></p>
+
+              <br class="clear">
+            </article>
+            <!-- /article -->
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <page-footer />
+  </div>
+</template>
