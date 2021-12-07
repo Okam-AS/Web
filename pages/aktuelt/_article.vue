b650592a7ed09ee0b7152159976bb64a80add108
Bård Øien <web.oien@gmail.com>
2021-12-07 15:43:52 +0100

    Update blog

--- pages/aktuelt/_article.vue
+++ pages/aktuelt/_article.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <page-header />
-    <main class="page-content">
+    <main class="page-content blog-post">
       <div class="article">
         <div class="wrapper">
           <div class="element text-content">
             <h1 class="heading-1 u-center">
               {{ article.heading }}
             </h1>
-            <img :src="article.main_image.formats.thumbnail.url" :alt="article.main_image.alternativeText">
-            <p> {{ article.main_intro }}</p>
-            <p> {{ article.main_body }}</p>
+            <img
+              class="blog-post__image"
+              :src="article.main_image.formats.small.url"
+              :alt="article.main_image.alternativeText"
+            >
+            <p>{{ article.main_intro }}</p>
+            <p>{{ article.main_body }}</p>
           </div>
         </div>
       </div>
@@ -21,7 +25,9 @@
 <script>
 export default {
   async asyncData ({ params, redirect }) {
-    const blogPosts = await fetch(`${process.env.STRAPI_BASE_URL}/blog-posts`).then(res => res.json())
+    const blogPosts = await fetch(
+      `${process.env.STRAPI_BASE_URL}/blog-posts`
+    ).then(res => res.json())
     const filteredPost = blogPosts.find(
       el => !!el.url && el.url.toLowerCase() === params.article
     )
@@ -58,3 +64,12 @@ export default {
   })
 }
 </script>
+
+<style lang="scss">
+.blog-post {
+	&__image {
+		margin-bottom: 3rem;
+		max-width: 100%;
+	}
+}
+</style>
--- pages/aktuelt/index.vue
+++ pages/aktuelt/index.vue
@@ -1,22 +1,32 @@
 <template>
   <div>
     <page-header />
-    <main class="page-content">
+    <main class="page-content blog">
       <div class="article">
         <div class="wrapper">
           <div class="element text-content">
             <h1 class="heading-1 u-center">
               {{ blogsRoot.heading }}
             </h1>
-            <p>{{ blogsRoot.main_intro }}</p>
+            <p class="u-center">
+              {{ blogsRoot.main_intro }}
+            </p>
             <div>
-              <a v-for="article in blogs" :key="article.id" :href="'/aktuelt/'+article.url">
-                <div style="border:1px solid black;">
-                  <h2>{{ article.heading }}</h2>
-                  <p>{{ article.main_intro }}</p>
-                  <img :src="article.main_image.formats.thumbnail.url" :alt="article.main_image.alternativeText">
-                </div>
-              </a>
+              <div v-for="article in blogs" :key="article.id">
+                <a :href="'/aktuelt/' + article.url" class="blog_item">
+                  <div class="blog_image">
+                    <img
+                      :src="article.main_image.formats.thumbnail.url"
+                      :alt="article.main_image.alternativeText"
+                    >
+                  </div>
+                  <div class="blog_teaser">
+                    <h2>{{ article.heading }}</h2>
+                    <p v-html="$md.render(article.main_intro)" />
+                    <a :href="'/aktuelt/' + article.url">Les mer</a>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -34,42 +44,74 @@ export default {
       heading: '',
       main_intro: ''
     },
-    blogs: [{
-      id: 0,
-      heading: '',
-      main_intro: '',
-      main_body: '',
-      main_image: {
-        alternativeText: '',
-        formats: {
-          small: {
-            url: ''
-          },
-          thumbnail: {
-            url: ''
+    blogs: [
+      {
+        id: 0,
+        heading: '',
+        main_intro: '',
+        main_body: '',
+        main_image: {
+          alternativeText: '',
+          formats: {
+            small: {
+              url: ''
+            },
+            thumbnail: {
+              url: ''
+            }
           }
-        }
-      },
-      images: [],
-      created_at: '',
-      updated_at: ''
-    }]
+        },
+        images: [],
+        created_at: '',
+        updated_at: ''
+      }
+    ]
   }),
   mounted () {
-    fetch(`${this.strapiBaseUrl}/blog-posts`)
-      .then((res) => {
-        res.json().then(
-          (jsonResponse) => {
-            this.blogs = jsonResponse
-          })
+    fetch(`${this.strapiBaseUrl}/blog-posts`).then((res) => {
+      res.json().then((jsonResponse) => {
+        this.blogs = jsonResponse
       })
-    fetch(`${this.strapiBaseUrl}/blog-posts-root`)
-      .then((res) => {
-        res.json().then(
-          (jsonResponse) => {
-            this.blogsRoot = jsonResponse
-          })
+    })
+    fetch(`${this.strapiBaseUrl}/blog-posts-root`).then((res) => {
+      res.json().then((jsonResponse) => {
+        this.blogsRoot = jsonResponse
       })
+    })
   }
 }
 </script>
+
+<style lang="scss">
+.blog {
+	&_item {
+		margin-top: 5rem;
+		display: flex;
+		flex-direction: column;
+		border-bottom: none;
+
+		@media only screen and (min-width: 600px) {
+			flex-direction: row;
+		}
+	}
+
+	&_image {
+		max-width: 100%;
+		@media only screen and (min-width: 600px) {
+			float: left;
+			max-width: 200px;
+		}
+
+		img {
+			width: 100%;
+			height: auto;
+		}
+	}
+
+	&_teaser {
+		@media only screen and (min-width: 600px) {
+			padding-left: 2rem;
+		}
+	}
+}
+</style>
