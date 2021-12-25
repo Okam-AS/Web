60cafda15fe1532e88e789d0a8e5c387b04e5eca
asharghi <a.sharghi.mail@gmail.com>
2021-12-25 16:39:38 +0100

    add, delete, copy row

--- pages/import.vue
+++ pages/import.vue
@@ -15,25 +15,27 @@
         </tr>
         <tr v-for="(row, index) in rows" :key="index">
           <td><autocomplete-input v-model="row.categoryName" :suggestions="['drikke', 'hamburger', 'pizza']" type="text" /></td>
-          <td><autocomplete-input v-model="row.productName" :suggestions="['cola 0.5L', 'fanta 0.5L', 'esso burger']" type="text" /></td>
-          <td><autocomplete-input v-model="row.productDescription" :suggestions="['her kommer en lengre beskrivelse som kan ta mye plass', 'fanta 0.5L', 'esso burger']" type="text" /></td>
+          <td><autocomplete-input v-model="row.name" :suggestions="['cola 0.5L', 'fanta 0.5L', 'esso burger']" type="text" /></td>
+          <td><autocomplete-input v-model="row.description" :suggestions="['her kommer en lengre beskrivelse som kan ta mye plass', 'fanta 0.5L', 'esso burger']" type="text" /></td>
           <td>
             <currency-input
+              v-model="row.price"
               currency="NOK"
               style="width:100px"
             />
           </td>
-          <td><input type="number" min="0" max="99"></td>
+          <td><input v-model="row.tax" type="number" min="0" max="99"></td>
           <td>
             <currency-input
+              v-model="row.depositPrice"
               currency="NOK"
               style="width:60px"
             />
           </td>
-          <td><input type="checkbox"></td>
-          <td>
-            <input class="emoji-btn" type="button" value="🔂 Dupliser rad" @click="deleteRow(key)">
-            <input class="emoji-btn" type="button" value="❌ Fjern rad" @click="deleteRow(key)">
+          <td><input v-model="row.isSoldOut" type="checkbox"></td>
+          <td v-if="index !== rows.length-1">
+            <input class="emoji-btn" type="button" value="🔂 Dupliser rad" @click="copyRow(index)">
+            <input class="emoji-btn" type="button" value="❌ Fjern rad" @click="deleteRow(index)">
           </td>
         </tr>
       </tbody>
@@ -55,15 +57,40 @@ export default {
   components: { Modal, AutocompleteInput },
   data: () => ({
     showModal: false,
-    rows: [{}, {}, {}, {}, {}, {}],
+    rows: [],
     tempSuggestions: ['test', 'asdas', 'sdfwfd']
   }),
   computed: {
     allKeys () {
       return ''
+    },
+    emptyRow () {
+      return {
+        categoryName: '',
+        name: '',
+        description: '',
+        price: undefined,
+        tax: 15,
+        depositPrice: undefined,
+        isSoldOut: false
+      }
+    }
+  },
+  watch: {
+    rows: {
+      handler (val) {
+        if (!Array.isArray(val) || val.length < 1) {
+          this.rows = [this.emptyRow]
+        } else if (!this.isEmptyRow(val[val.length - 1])) {
+          this.addEmptyRow()
+        }
+      },
+      deep: true
     }
   },
   mounted () {
+    this.addEmptyRow()
+    this.addEmptyRow()
     // fetch(`${this.strapiBaseUrl}/about-us`)
     //   .then((res) => {
     //     res.json().then(
@@ -73,11 +100,24 @@ export default {
     //   })
   },
   methods: {
-    deleteRow () {
-
+    isEmptyRow (row) {
+      if (!row) { return true }
+      const _this = this
+      let isEmptyRow = true
+      Object.keys(this.emptyRow).forEach((key) => {
+        if (row[key] !== _this.emptyRow[key]) { isEmptyRow = false }
+      })
+      return isEmptyRow
     },
-    changing (event) {
-      console.log(event)
+    deleteRow (index) {
+      this.$delete(this.rows, index)
+    },
+    addEmptyRow () {
+      this.rows.push(JSON.parse(JSON.stringify(this.emptyRow)))
+    },
+    copyRow (index) {
+      const copiedRow = JSON.parse(JSON.stringify(this.rows[index]))
+      this.rows.splice(index, 0, copiedRow)
     }
   }
 }
