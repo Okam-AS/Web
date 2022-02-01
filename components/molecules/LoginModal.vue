1ac0898e8d8aa7b88f9d9f50a929ebe88a0058dc
sveibern <sveinung@hverdag.no>
2022-02-01 22:44:36 +0100

    Startet på styling av login

--- components/molecules/LoginModal.vue
+++ components/molecules/LoginModal.vue
@@ -1,38 +1,43 @@
 <template>
   <Modal @close="close">
-    <Loading :loading="isLoading" />
-    <template v-if="!isLoading">
-      <h1 style="margin-bottom:1em">
-        {{ $t('login') }}
-      </h1>
-      <div v-if="user && user.token">
-        {{ $t('youAreLoggedIn') + ' ' + user.phoneNumber }}
-        <div>
-          <input type="button" class="emoji-btn" style="margin-top:1em" :value="$t('close')" @click="close">
-          <input type="button" class="emoji-btn" style="margin-top:1em" :value="$t('logout')" @click="wipeUser">
+    <div class="shop__content">
+      <Loading :loading="isLoading" />
+      <template v-if="!isLoading">
+        <h1 class="shop__heading">
+          {{ $t('login') }}
+        </h1>
+        <div v-if="user && user.token">
+          {{ $t('youAreLoggedIn') + ' ' + user.phoneNumber }}
+          <div>
+            <input type="button" class="emoji-btn" :value="$t('close')" @click="close">
+            <input type="button" class="emoji-btn"  :value="$t('logout')" @click="wipeUser">
+          </div>
         </div>
-      </div>
-      <div v-else>
-        <template v-if="!smsSent">
-          <p>{{ $t('enterPhoneNumberLabel') }}</p>
-          <input
-            v-model="phone"
-            :placeholder="$t('enterPhoneNumberPlaceholder')"
-            type="text"
-            @keyup.enter="getCode"
-          >
-          <input type="button" class="emoji-btn" :value="$t('enterPhoneNumberSubmit')" @click="getCode">
-        </template>
-        <template v-else-if="!codeSent">
-          <p>{{ $t('enterPhoneCodeLabel') }}</p>
-          <div>{{ phone }} <input type="button" style="font-size:10px;margin-bottom:16px" value="✏️" @click="reset"></div>
-          <OtpInput loading="true" @complete="login" />
-        </template>
-        <p v-if="errorMessage" class="error-message">
-          {{ errorMessage }}
-        </p>
-      </div>
-    </template>
+        <div class="m-t-xs" v-else>
+          <template v-if="!smsSent">
+            <p>{{ $t('enterPhoneNumberLabel') }}</p>
+            <div class="input-btn-row">
+              <input
+                v-model="phone"
+                :placeholder="$t('enterPhoneNumberPlaceholder')"
+                type="text"
+                @keyup.enter="getCode"
+                class="input"
+              >
+              <input type="button" class="btn btn--primary" :value="$t('enterPhoneNumberSubmit')" @click="getCode">
+            </div>
+          </template>
+          <template v-else-if="!codeSent">
+            <p>{{ $t('enterPhoneCodeLabel') }}</p>
+            <OtpInput loading="true" @complete="login" />
+            <div>{{ phone }} <input type="button" value="✏️" @click="reset"></div>
+          </template>
+          <p v-if="errorMessage" class="error-message">
+            {{ errorMessage }}
+          </p>
+        </div>
+      </template>
+    </div>
   </Modal>
 </template>
 <script>
