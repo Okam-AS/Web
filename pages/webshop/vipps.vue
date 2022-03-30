728b5a6aece871e0f3afebfcdfa5f96009824e27
asharghi <a.sharghi.mail@gmail.com>
2022-03-31 01:24:59 +0200

    Vipps landingpage

--- pages/webshop/vipps.vue
+++ pages/webshop/vipps.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <Loading />
+    <div v-if="showSuccessResponse || showFailResponse">
+      <span>{{ showSuccessResponse ? "Success" : "Fail" }}</span>
+    </div>
   </div>
 </template>
 
@@ -9,28 +12,73 @@ import Loading from '@/components/atoms/Loading.vue'
 
 export default {
   components: { Loading },
+  data: () => ({
+    vippsOrderId: '',
+    showSuccessResponse: false,
+    showFailResponse: false
+  }),
+  computed: {
+    userIsLoggedIn () {
+      return this.$store.getters.userIsLoggedIn
+    }
+  },
   mounted () {
-    if (this.storeId) {
-      this._cartService
-        .Complete(this.storeId)
-        .then(() => {
-          this.redirectToSuccess()
-        })
-        .catch(() => {
-          this.redirectToError()
-        })
+    const search =
+      window && window.location
+        ? new URLSearchParams(window.location.search) || {}
+        : {}
+
+    this.vippsOrderId = search.has('vippsOrderId')
+      ? decodeURI(search.get('vippsOrderId'))
+      : ''
+
+    if (this.vippsOrderId) {
+      setTimeout(() => {
+        const intervalId = setInterval(() => {
+          this._vippsService
+            .Verify(this.vippsOrderId)
+            .then((result) => {
+              // result.status: 'Waiting', 'Success', or 'Fail'
+              if (result.status === 'Success') {
+                clearInterval(intervalId)
+                this.handleSuccess(result.storeId)
+              } else if (result.status === 'Fail') {
+                clearInterval(intervalId)
+                this.handleError(result.storeId)
+              }
+            })
+            .catch(() => {
+              clearInterval(intervalId)
+              this.handleError()
+            })
+        }, 2000)
+      }, 3000)
     } else {
-      this.redirectToError()
+      this.handleError()
     }
   },
   methods: {
-    redirectToSuccess () {
-      this.paymentStatus = 'success'
-      window.location.href = '/webshop/orders' + this.urlQueryStrings
+    handleSuccess (storeId) {
+      if (this.userIsLoggedIn && storeId) {
+        this.redirectToSuccess(storeId)
+      } else {
+        this.showSuccessResponse = true
+      }
     },
-    redirectToError () {
-      this.paymentStatus = 'failed'
-      window.location.href = '/webshop/checkout' + this.urlQueryStrings
+    handleError (storeId) {
+      if (this.userIsLoggedIn && storeId) {
+        this.redirectToError(storeId)
+      } else {
+        this.showFailResponse = true
+      }
+    },
+    redirectToSuccess (storeId) {
+      window.location.href =
+        '/webshop/orders?store=' + storeId + '&paymentStatus=success'
+    },
+    redirectToError (storeId) {
+      window.location.href =
+        '/webshop/checkout?store=' + storeId + '&paymentStatus=failed'
     }
   }
 }
