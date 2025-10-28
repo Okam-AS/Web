cb76a6f2261011478f39dfcd1c0338840f5d6ac0
asharghi <a.sharghi.mail@gmail.com>
2025-10-28 16:52:51 +0100

    Fix eta

--- pages/admin/orders.vue
+++ pages/admin/orders.vue
@@ -800,6 +800,9 @@ export default {
         return `⏱️ ${diffMinutes} min`;
       } else {
         return eta.toLocaleTimeString("no-NO", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
           hour: "2-digit",
           minute: "2-digit",
         });
