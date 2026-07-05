bc5afd15f7bfc1aa489ed3a9502c327cc34f00d1
Sven Daneel <sven4696@gmail.com>
2026-07-05 11:32:23 +0200

    test(admin): cover StoreLogoCard.upload network path

--- test/StoreLogoCard.spec.js
+++ test/StoreLogoCard.spec.js
@@ -1,4 +1,7 @@
+jest.mock('axios')
+
 import { shallowMount } from '@vue/test-utils'
+import axios from 'axios'
 import StoreLogoCard from '~/components/admin/StoreLogoCard.vue'
 
 function mountCard (getImpl) {
@@ -40,5 +43,37 @@ describe('StoreLogoCard', () => {
     const spy = jest.spyOn(wrapper.vm, 'cropAndUpload').mockImplementation(() => {})
     wrapper.vm.handleFile({ type: 'image/png', size: 1024 })
     expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  test('upload() posts the cropped blob and reports success', async () => {
+    axios.post.mockResolvedValue({})
+    const { wrapper, showToast } = mountCard()
+
+    wrapper.vm.upload(new Blob(['x'], { type: 'image/png' }))
+    await wrapper.vm.$nextTick()
+    await wrapper.vm.$nextTick()
+
+    expect(axios.post).toHaveBeenCalledTimes(1)
+    const [url, formData, config] = axios.post.mock.calls[0]
+    expect(url).toBe('http://api.test/stores/logo')
+    expect(formData).toBeInstanceOf(FormData)
+    expect(formData.get('NumberId')).toBe('42')
+    expect(formData.get('Image')).toBeInstanceOf(Blob)
+    expect(config).toEqual({ headers: { Authorization: 'Bearer t' } })
+
+    expect(showToast).toHaveBeenCalledWith('logo_updated', 'success')
+    expect(wrapper.emitted('logo-updated')).toBeTruthy()
+  })
+
+  test('upload() shows an error toast and does not emit when the request fails', async () => {
+    axios.post.mockRejectedValue(new Error('boom'))
+    const { wrapper, showToast } = mountCard()
+
+    wrapper.vm.upload(new Blob(['x'], { type: 'image/png' }))
+    await wrapper.vm.$nextTick()
+    await wrapper.vm.$nextTick()
+
+    expect(showToast).toHaveBeenCalledWith('logo_errorUpload', 'error')
+    expect(wrapper.emitted('logo-updated')).toBeFalsy()
   })
 })
